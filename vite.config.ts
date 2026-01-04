53b1ee6fe634d2dd7a3df7d20a8e3fdb54c94962
LIN <shenglin.sei@gmail.com>
2026-01-04 22:23:34 +0900

    修复404

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
-  // ⭐ GitHub Pages 子路径
-  base: '/jici/',
-
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-
-      manifest: {
-        name: '积词',
-        short_name: '积词',
-
-        // ⭐⭐ 核心：必须是 /jici/
-        start_url: '/jici/',
-        scope: '/jici/',
-
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-
-        icons: [
-          {
-            src: '/jici/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/jici/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-});
+base: '/jici/',
+plugins: [
+tailwindcss(),
+react(),
+VitePWA({
+registerType: 'autoUpdate',
+manifest: {
+name: '积词',
+short_name: '积词',
+start_url: '/jici/',
+scope: '/jici/',
+display: 'standalone',
+background_color: '#ffffff',
+theme_color: '#53BEE8',
+icons: [
+{ src: '/jici/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+{ src: '/jici/pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+],
+},
+}),
+],
+})
